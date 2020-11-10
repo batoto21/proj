9faa2a6beffd8c3e68192aa3fc35271de739cc71
Angelica Batoto <angelicabatoto21@gmail.com>
2020-11-10 12:18:11 -0800

    First Commit

---
+++ add.php
@@ -0,0 +1,69 @@
+<html>
+<head>
+	<title>Add Data</title>
+</head>
+
+<body>
+<?php
+//including the database connection file
+include_once("config.php");
+
+if(isset($_POST['Submit'])) {	
+	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
+	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
+	$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+		
+	// checking empty fields
+	if(empty($name) || empty($age) || empty($email)) {
+				
+		if(empty($name)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+		
+		if(empty($age)) {
+			echo "<font color='red'>Age field is empty.</font><br/>";
+		}
+		
+		if(empty($email)) {
+			echo "<font color='red'>Email field is empty.</font><br/>";
+		}
+		
+		//link to the previous page
+		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+	} else { 
+		// if all the fields are filled (not empty) 
+			
+		//insert data to database	
+		$result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$age','$email')");
+		
+		//display success message
+		echo "<font color='green'>Data added successfully.";
+		echo "<br/><a href='index.php'>View Result</a>";
+	}
+}
+?>
+</body>
+</html>
+
+<?php
+/*
+// mysql_connect("database-host", "username", "password")
+$conn = mysql_connect("localhost","root","root") 
+			or die("cannot connected");
+// mysql_select_db("database-name", "connection-link-identifier")
+@mysql_select_db("test",$conn);
+*/
+
+/**
+ * mysql_connect is deprecated
+ * using mysqli_connect instead
+ */
+
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'test';
+$databaseUserName = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUserName, $databasePassword, $databaseName);
+?>
